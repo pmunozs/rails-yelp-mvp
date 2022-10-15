bd2b772a114ab3f76a2172523f0f182be8f8bb61
Pablo Munoz <pablo.munoz.sommaruga@gmail.com>
2022-10-15 11:15:13 +0200

    Restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'restaurants#index'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Clearing old database entries....'
+Restaurant.destroy_all
+
+puts 'Seeding...'
+categories = %w[chinese italian japanese french belgian]
+
+5.times do
+  restaurant = Restaurant.create({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories[rand(0...5)]
+  })
+  restaurant.save
+end
+
+puts 'Enjoy, my friend 😉'
